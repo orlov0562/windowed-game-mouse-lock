219893d4ef0299a352812c5ee44e266f178f177c
Vitaliy S. Orlov <orlov0562@gmail.com>
2023-01-31 14:38:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const x11 = require("x11");
 const {promisify} = require("util");
 
-function clamp(min, n, max) {
-    n = Math.max(min, n);
-    n = Math.min(max, n);
-    return n;
-}
+let wx = 1920;
+
+let argv = process.argv.slice(2);
+if (argv[0]!=undefined) wx = argv[0];
 
 x11.createClient(async function(err, display) {
     const X = display.client;
@@ -13,33 +12,7 @@ x11.createClient(async function(err, display) {
 
     const queryPointer = promisify(X.QueryPointer.bind(X));
     const getFocus = promisify(X.GetInputFocus.bind(X));
-    const getAttributes = promisify(X.GetWindowAttributes.bind(X));
-    const getGeometry = promisify(X.GetGeometry.bind(X));
-    const queryTree = promisify(X.QueryTree.bind(X));
     const timeout = promisify(setTimeout);
-
-    async function getGeometryInRoot(window) {
-
-        let ret = await getGeometry(window);
-
-        while(true) {
-
-            let qtRes = await queryTree(window);
-
-            if(qtRes.root == qtRes.parent) {
-                break;
-            }
-
-            window = qtRes.parent;
-
-            let geo = await getGeometry(window);
-
-            ret.xPos += geo.xPos;
-            ret.yPos += geo.yPos;
-        }
-
-        return ret;
-    }
 
     let i = 6;
     console.log("getting window in: ");
@@ -58,18 +31,13 @@ x11.createClient(async function(err, display) {
 
         if(currentWindow == targetWindow) {
 
-            let geo = await getGeometryInRoot(targetWindow);
-
             let ptr = await queryPointer(root);
 
-            let x = ptr.childX;
-            let y = ptr.childY;
-
-            let cx = clamp(geo.xPos, x, geo.xPos + geo.width);
-            let cy = clamp(geo.yPos, y, geo.yPos + geo.height);
-
-            if(cx != x || cy != y) {
-                X.WarpPointer(0, root, 0, 0, 0, 0, cx, cy);
+            let px = ptr.childX;
+            let py = ptr.childY;
+            
+            if (px > wx) {
+                X.WarpPointer(0, root, 0, 0, 0, 0, wx*2, py);
             }
         }
 
